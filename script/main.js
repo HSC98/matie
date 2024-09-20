63188c003f693e548b2ef5f9e957ff0a88317a07
HSC98 <gktmdcjf98@gmail.com>
2024-09-20 14:39:53 +0900

    7

--- script/main.js
+++ script/main.js
@@ -1,8 +1,8 @@
 
   $(document).ready(function(){
       $('.slider').slick({
-          prevArrow: '<button type="button" class="slick-prev"><img src="../img/왼쪽화살표.png" alt="Previous"></button>',
-          nextArrow: '<button type="button" class="slick-next"><img src="../img/오른쪽화살표.png" alt="Next"></button>',
+          prevArrow: '<button type="button" class="slick-prev"><img src="img/왼쪽화살표.png" alt="Previous"></button>',
+          nextArrow: '<button type="button" class="slick-next"><img src="img/오른쪽화살표.png" alt="Next"></button>',
           centerMode: true,        
           centerPadding: '30%',    
           slidesToShow: 1,         
